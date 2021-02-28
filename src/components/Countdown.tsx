dd217e26a4f190ec8f6eb1ebd4b460e2e948e25f
Alisson Rodrigo da Silva <66378121+Alissonpgd@users.noreply.github.com>
2021-02-28 15:08:32 -0300

    Update Countdown.tsx

--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -1,6 +1,6 @@
 import { useContext } from 'react';
 import {CountdownContext} from '../contexts/CountdownContext';
-import styles from '../styles/components/Countdown.module.css';
+import styles from '../styles/components/CountDown.module.css';
 
 export function Countdown() {
 
